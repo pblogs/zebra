da47163347d7085e0dd9f803439fce2ca49d0999
Rosca Sergiu <rmagnum2002@gmail.com>
2015-06-13 22:02:19 +0300

    assets upload hack

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,9 +1,4 @@
 class Invoice < ActiveRecord::Base
-  # attr_accessible :amount, :customer_name,
-  #       :customer_notes, :due_date, :emails,
-  #       :invoice_date, :invoice_number,
-  #       :order_number, :status,
-  #       :terms_and_cond
   has_many :invoice_items
   has_many :assets, as: :attachable, dependent: :destroy
   accepts_nested_attributes_for :invoice_items, allow_destroy: true
